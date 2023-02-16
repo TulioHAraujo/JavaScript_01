a7c98e5902b29d847a1a64214c9a9c52ba2640e4
Túlio Araujo <tulioharaujoo@gmail.com>
2023-02-16 11:20:03 -0300

    estudo
    revisando

--- Curso_JS/aula11/ex009.js
+++ Curso_JS/aula11/ex009.js
@@ -1,7 +1,7 @@
 var pais = 'Argentina'
 console.log(`Se você mora no(a) ${pais}`)
-if (pais == 'Brasil') { //condição composta
-    console.log('Você é Brasileiro !')
-} else {
+if (pais != 'Brasil') { //condição composta
     console.log('Você é estrangeiro !')
+} else {
+    console.log('Você é Brasileiro !')
 }
